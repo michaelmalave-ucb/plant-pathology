137effcd4f40ed0491e667e0f2e83b7d628c9a69
michaelmalave-ucb <michaelmalave@berkeley.edu>
2021-10-31 12:35:07 -0700

    updating processing to remove test load

--- processing.py
+++ processing.py
@@ -27,13 +27,11 @@ for i_list in batch(train_labels, chunk):
     for i in i_list:
         count += 1
         image = Image.open(img_dir + i)
-        # print(str(image.shape[0]) + " " + str(image.shape[1]) + " " + str(image.shape[2]))
         # this should be resizing the files to all the same dimensions
         image.thumbnail(size=(200, 400), resample=1)
         im = np.array(image)
         # limiting to only images that fit shape for now
         if im.shape[0] == 134:
-            # print(str(im.shape[0]) + " " + str(im.shape[1]) + " " + str(im.shape[2]))
             im = list(im.reshape(im.shape[0] * im.shape[1] * im.shape[2]).tolist())
             # append list as new row to csv
             with open("train_data.csv", "a+") as f:
@@ -48,6 +46,7 @@ for i_list in batch(train_labels, chunk):
         if count % 50 == 0:
             print("Completed items: " + str(count))
         image.close()
+    # This limits to a subset of the data to be added to the file
     if count == 500:
         break
 
@@ -56,14 +55,3 @@ end = time.time()
 print('Process Complete')
 total_time = round(end - start, 2)
 print("Total time was: " + str(total_time) + " seconds.")
-
-start = time.time()
-with open("train_data.csv", 'r') as f:
-    my_data = np.genfromtxt(f, delimiter=',')
-f.close()
-end = time.time()
-total_time = round(end - start, 2)
-print("Loading time was: " + str(total_time) + " seconds.")
-
-print('data type: ', type(my_data))
-print('data shape: ', my_data.shape)

05efb8c3f33add7d9ddc5e7d2bb8650a1d6023d1
michaelmalave-ucb <michaelmalave@berkeley.edu>
2021-10-31 09:11:51 -0700

    adding processing file for image data and project file for ML

---
+++ processing.py
@@ -0,0 +1,58 @@
+import csv
+import time
+import numpy as np
+from glob import glob
+from PIL import Image
+
+
+def batch(iterable, n=1):
+    l = len(iterable)
+    for ndx in range(0, l, n):
+        yield iterable[ndx:min(ndx + n, l)]
+
+
+chunk = 50
+img_dir = 'data/train_images/'
+image_files = glob(img_dir + '*.jpg')
+train_labels = np.loadtxt('data/train.csv', delimiter=',', skiprows=1, usecols=0, dtype=str)
+
+# you can use PIL or matplotlib for image loading. PIL seems very common
+# load data in same order as the data is labeled
+
+start = time.time()
+count = 0
+square = 5
+for i_list in batch(train_labels, chunk):
+
+    im_arr = []
+    for i in i_list:
+        count += 1
+        image = Image.open(img_dir + i)
+        image.thumbnail(size=(200, 400), resample=1)
+        im = np.array(image)
+        if im.shape[0] != 134:
+            print(str(im.shape[0]) + " " + str(im.shape[1]) + " " + str(im.shape[2]))
+        im = list(im.reshape(im.shape[0] * im.shape[1] * im.shape[2]).tolist())
+        """if len(im) != 45:
+            print(len(im))"""
+        with open("train_data.csv", "a+") as f:
+            write = csv.writer(f)
+            write.writerow(im)
+        image.close()
+        if count % 50 == 0:
+            print("Completed items: " + str(count))
+    if count > 100:
+        break
+
+
+end = time.time()
+print('Process Complete')
+total_time = round(end - start, 2)
+print("Total time was: " + str(total_time) + " seconds.")
+
+
+with open('train_data.csv', 'r') as r:
+    csv_reader = csv.reader(r)
+    list_of_rows = list(csv_reader)
+
+
---
+++ project.py
@@ -0,0 +1,66 @@
+import time
+import numpy as np
+from csv import reader
+from sklearn.pipeline import Pipeline
+from sklearn.datasets import fetch_openml
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LinearRegression
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report
+
+start = time.time()
+print("Starting loading data")
+X = np.loadtxt('train_data.csv', delimiter=',')
+Y = np.loadtxt('data/train.csv', delimiter=',', skiprows=1, usecols=0, dtype=str)
+print("Finished loading data")
+end = time.time()
+print('Process Complete')
+total_time = round(end - start, 2)
+print("Total time loading data was: " + str(total_time) + " seconds.")
+
+
+print('per data shape: ', len(X[0]))
+print('data shape: ', X.shape)
+print('label shape:', Y.shape)
+
+shuffle = np.random.permutation(np.arange(X.shape[0]))
+X, Y = X[shuffle], Y[shuffle]
+
+
+# Fractionally distribute subsets of data
+big_size = round(X.shape[0]*0.8)
+small_size = round(X.shape[0]*0.02)
+
+for i in X:
+    print(i)
+
+# Set some variables to hold test, dev, and training data.
+test_data, test_labels = X[big_size:], Y[big_size:]
+dev_data, dev_labels = X[big_size-small_size:big_size], Y[big_size-small_size:big_size]
+train_data, train_labels = X[:big_size-small_size], Y[:big_size-small_size]
+mini_train_data, mini_train_labels = X[:small_size], Y[:small_size]
+
+
+def q2(k_values):
+  for k in k_values:
+    # Produce models for each k value
+    if k == k_values[0]:
+      print("1. Producing models for where k={k_values}".format(k_values=k_values))
+      print("2.")
+    knn = KNeighborsClassifier(n_neighbors=k)
+    knn.fit(train_data, train_labels)
+    # get predictions for dev data to be evaluated
+    pred = knn.predict(dev_data)
+    score = knn.score(dev_data, dev_labels)
+    print("Accuracy with k={k}: {score}".format(k=k, score=score))
+    if k == k_values[-1]:
+      report = classification_report(pred, dev_labels)
+      print("3. Classification report for where k={k}".format(k=k))
+      print(report)
+
+
+k_vals = [1, 3, 5, 7, 9]
+q2(k_vals)

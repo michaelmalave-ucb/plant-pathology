b1be3454e5c204ba1d7ae827901406a7b883086d
Mili Gera <miligera@berkeley.edu>
2021-12-06 06:11:17 -0800

    updating all files for prep to merge

--- project_mg_logreg.py
+++ project_mg_logreg.py
@@ -1,3 +1,5 @@
+# Import all necessary packages and modules
+
 import time
 import numpy as np
 import pandas as pd
@@ -20,15 +22,12 @@ from sklearn.model_selection import train_test_split
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
+from sklearn.decomposition import PCA, IncrementalPCA
 
-
-
+# Just a timer to see load process
 start = time.time()
-print("Starting loading data")
+print("Starting loading data into X")
 
-# this method is a little slower than read_csv so we can remove
-#with open("train_data.csv", 'r') as f:
-  #X = np.genfromtxt(f, delimiter=',')
 
 # Both pandas.DataFrame and pandas.Series have values attribute that returns
 #NumPy array numpy.ndarray. After pandas 0.24.0, it is recommended to use the to_numpy()
@@ -40,7 +39,7 @@ X =pd.read_csv("train_data.csv", delimiter=",").values
 # get all image names and labels and put into pandas dataframe for further processing
 # explicitly state headers for all six possibilities
 all_labels = pd.read_csv("data/train_labels_final_single.csv",names=["image_name", "col1", "col2", "col3", "col4", "col5", "col6"], delimiter=",")
-print("two header rows", all_labels.head(5))
+
 
 # empty list where we will turn the label data into binary 0s and 1s to use Binary Relevance
 all_rows = []
@@ -78,17 +77,12 @@ for row in all_labels.itertuples(index=False, name='Labels'):
         each_row.append(1)
     else:
         each_row.append(0)
-    print(row[:])
-    print(each_row)
+
     all_rows.append(each_row)
-print(all_rows[1])
+
 # pop the row which is not the old header
 all_rows.pop(1)
 
-
-
-# use this code if you want to do single-label
-#Y = np.loadtxt('data/train_labels_final_single.csv', delimiter=',', skiprows=1, usecols=1, dtype=str)
 
 # use this code if you want to do binary Relevance, Label Powerset or Classifier Chain
 # create a pandas dataframe of all binarized label data to prep our expected output
@@ -96,12 +90,13 @@ all_rows.pop(1)
 y = pd.DataFrame(all_rows[1:], columns=all_rows[0])
 
 Y = y[['scab', 'healthy', 'frog_eye_leaf_spot','rust','complex','powdery_mildew']]
-Y.to_csv('out.csv')
-print("Saved out file")
 
+# Create an out file to make sure you created the correct binarization for each label.
+Y.to_csv('out.csv')
+print("Saved out file as a sanity check on binarizing each label")
 print("Finished loading data")
 end = time.time()
-print('Process Complete')
+print('Process of Loading X and Y Complete')
 total_time = round(end - start, 2)
 print("Total time loading data was: " + str(total_time) + " seconds.")
 
@@ -116,6 +111,7 @@ print("Starting split of train and dev data")
 train_data,dev_data,train_labels,dev_labels = train_test_split(X,Y,test_size=0.2,random_state=42)
 
 # some further sanity checks to make sure we have the right shapes after we do the split
+print("Shapes of data after train, dev split are as follows:")
 print("Train data shape", train_data.shape)
 print("Train label shape", train_labels.shape)
 print("Dev data shape", dev_data.shape)
@@ -123,33 +119,51 @@ print("Dev label shape", dev_labels.shape)
 
 print("Ending split of train and dev data")
 
+# Lets see the category distribution in the train and dev sets
+
+print("The number of samples in the train data for each category are as follows:")
 
 
-# BinaryRelevance with Logistic Regression Does Not Converge With Any Kind of Setting
-# Will need to try and do PCA.
+print("Train data scab counts", (train_labels.scab == 1).sum())
+print("Train data healthy counts", (train_labels.healthy == 1).sum())
+print("Train data frog_eye_leaf_spot counts", (train_labels.frog_eye_leaf_spot == 1).sum())
+print("Train data rust counts", (train_labels.rust == 1).sum())
+print("Train data complex counts", (train_labels.complex == 1).sum())
+print("Train data powdery_mildew counts", (train_labels.powdery_mildew == 1).sum())
 
-#def logreg():
-
-    #parameters = [
-    #{
-        #'classifier': [LogisticRegression(multi_class="multinomial", tol=0.015)],
-        #'classifier__C': [2.0, 10.0, 100],
-        #'classifier__max_iter': [1000]
-    #},
-
-    #]
-
-    #clf = GridSearchCV(BinaryRelevance(), parameters, cv = 5, scoring='f1_micro')
-    #print("Starting Fit")
-    #clf.fit(train_data, train_labels)
-
-    #print (clf.best_params_, clf.best_score_)
-
-#logreg()
+print("The number of samples  in the dev data for each category are as follows:")
+print("Dev data scab counts", (dev_labels.scab == 1).sum())
+print("Dev data healthy counts", (dev_labels.healthy == 1).sum())
+print("Dev data frog_eye_leaf_spot counts", (dev_labels.frog_eye_leaf_spot == 1).sum())
+print("Dev data rust counts", (dev_labels.rust == 1).sum())
+print("Dev data complex counts", (dev_labels.complex == 1).sum())
+print("Dev data powdery_mildew counts", (dev_labels.powdery_mildew == 1).sum())
 
 
+# We will probably need to do PCA for Logistic Regression as it will not Converge
+# so lets take the X data and the binarized Y label data and first scale it and then do PCA
+
+
+# scale all the data
+sc = StandardScaler()
+x_scaled = sc.fit_transform(X)
+print("Scaling of data done")
+# Do some sanity check
+print(x_scaled.shape)
+print(x_scaled[0:10])
+
+# Principal component analysis using scaled data.
+# Landed on n_components 50 after checking for various options and looking at variance capture
+pca = PCA(n_components=200)
+x_pca = pca.fit_transform(x_scaled)
+print ("PCA transform done")
+
+# split the pca data into train and dev set
+print("Starting split of train pca and dev pca data")
+train_data_pca,dev_data_pca,train_labels_pca,dev_labels_pca = train_test_split(x_pca,Y,test_size=0.2,random_state=42)
+
+# We tried a One vs Rest classifier in our case. It is similar to Binary Relevance. However this also did not converge
 def ovr():
-
 
     clf_ovr = OneVsRestClassifier(LogisticRegression(penalty='l1', class_weight='balanced', solver="liblinear"), n_jobs=-1)
     clf_ovr.fit(train_data, train_labels)
@@ -162,7 +176,7 @@ def ovr():
     precision = precision_score(dev_labels, predictions, average='micro')
     recall = recall_score(dev_labels, predictions, average='micro')
     f1 = f1_score(dev_labels, predictions, average='micro')
-    print("\nMicro-average quality numbers")
+    print("\nMicro-average quality numbers for One Vs Rest Logistic Regression")
     print("Precision: {:.4f}, Recall: {:.4f}, F1-measure: {:.4f}".format(precision, recall, f1))
 
     precision = precision_score(dev_labels, predictions, average='macro')
@@ -171,92 +185,222 @@ def ovr():
     print("\nMacro-average quality numbers")
     print("Precision: {:.4f}, Recall: {:.4f}, F1-measure: {:.4f}".format(precision, recall, f1))
 
-    print("\nClassification Report")
+    print("\nClassification Report for One Versus Rest Logistic Regression")
     print (metrics.classification_report(dev_labels, predictions))
-
 
 #ovr()
 
+# Next lets see if we can do some dimenstionality reduction
+# Only need to run dim_reduce function once. This is just to help determine
+# n_components for PCA reduction
 def dim_reduce():
 
-# Do feature scaling. From this the fraction of total variance for the first k
-# principle components 25 is 87% and 50 is 1
 
-  sc = StandardScaler()
-  x_scaled = sc.fit_transform(X)
-  print("Scaled Data")
-
-  # Principal component analysis using scaled train data
-  pca = PCA(n_components=50)
-  pca.fit(x_scaled)
-
+  # Will need to do incremenalPCA because PCA to figure out ideal n_components
+  # will not work on this large dataset
+  print("Starting Incremental PCA")
+  n = x_scaled.shape[0]
+  chunk_size = 4000
+  ipca = IncrementalPCA(n_components =  2000)
+  for i in range(0, n//chunk_size):
+    ipca.partial_fit(x_scaled[i*chunk_size : (i+1)*chunk_size])
+  print("Ending Incremental PCA")
 
   # Get the explained variance array. Can also use explained_variance_ratio
-  pca_variance = pca.explained_variance_
+  ipca_variance = ipca.explained_variance_
 
   # Get fraction of total variance explained by first k principal components
-  for k in (25,50):
-    print("Fraction of total variance for the first", k, "principal components:",
-          sum(pca_variance[0:k])/sum(pca_variance[0:]))
+  for k in (25,50,60,200,1000):
+   print("Fraction of total variance for the first", k, "principal components:",
+         sum(ipca_variance[0:k])/sum(ipca_variance[0:]))
   print("\n\n")
+
+
+  # KEEP THE RESULTS BELOW
+  # Results of Incremental PCA used to do dimensionality reduction.
+  #Fraction of total variance for the first 25 principal components: 0.5898387666140356
+  #Fraction of total variance for the first 50 principal components: 0.6778640348589169
+  #Fraction of total variance for the first 60 principal components: 0.7000517524379772
+  #Fraction of total variance for the first 200 principal components: 0.8312620486174991
+  #Fraction of total variance for the first 1000 principal components: 0.9651329500281319
+
+  # Principal component analysis using scaled train data from up above
+  #pca_test = PCA()
+  #pca_test.fit(x_scaled)
+
+  # Get the explained variance array. Can also use explained_variance_ratio
+  #pca_variance = pca_test.explained_variance_
+
+  # Get fraction of total variance explained by first k principal components
+  #for k in (25,50,60,200,1000):
+    #print("Fraction of total variance for the first", k, "principal components:",
+          #sum(pca_variance[0:k])/sum(pca_variance[0:]))
+  #print("\n\n")
+
+  #Do a sanity check on the variance capture calculations
+  #print(pca_test.explained_variance_ratio_.cumsum())
 
 #dim_reduce()
 
+# Now try One Versus Rest Logistic Regression Using PCA'd data and we will Grid
+# Search for best parameters to use with Binary Relevance Logistic Regression, Label
+# Powerset Logistic Regression and Class Chains Logistic Regression.
+
 def dim_reduct_transform():
 
-      # scale all the data
-      sc = StandardScaler()
-      x_scaled = sc.fit_transform(X)
-
-      # Principal component analysis using scaled data
-      pca = PCA(n_components=50)
-      x_pca = pca.fit_transform(x_scaled)
-      print ("PCA transform done")
-
-      # split the pca data
-      print("Starting split of train pca and dev pca data")
-      train_data_pca,dev_data_pca,train_labels_pca,dev_labels_pca = train_test_split(x_pca,Y,test_size=0.2,random_state=42)
 
       clf_ovr = OneVsRestClassifier(LogisticRegression(penalty='l1', class_weight='balanced', solver="liblinear"), n_jobs=-1)
       clf_ovr.fit(train_data_pca, train_labels_pca)
-      print("OVR fit done")
+      print("OVR Logistic Regression PCA'd data fit done")
 
       predictions = clf_ovr.predict(dev_data_pca)
 
-      print("Accuracy :",metrics.accuracy_score(dev_labels_pca, predictions))
-      print("Hamming loss ",metrics.hamming_loss(dev_labels_pca,predictions))
+      print("Accuracy for OVR Logistic Regression PCA'd data :",metrics.accuracy_score(dev_labels_pca, predictions))
+      print("Hamming loss for OVR Logistic Regression PCA'd data: ",metrics.hamming_loss(dev_labels_pca,predictions))
 
       precision = precision_score(dev_labels_pca, predictions, average='micro')
       recall = recall_score(dev_labels_pca, predictions, average='micro')
       f1 = f1_score(dev_labels_pca, predictions, average='micro')
-      print("\nMicro-average quality numbers")
+      print("\nMicro-average quality numbers for OVR Logistic Regression PCA'd data:")
       print("Precision: {:.4f}, Recall: {:.4f}, F1-measure: {:.4f}".format(precision, recall, f1))
 
-      precision = precision_score(dev_labels_pca, predictions, average='macro')
-      recall = recall_score(dev_labels_pca, predictions, average='macro')
-      f1 = f1_score(dev_labels_pca, predictions, average='macro')
-      print("\nMacro-average quality numbers")
-      print("Precision: {:.4f}, Recall: {:.4f}, F1-measure: {:.4f}".format(precision, recall, f1))
+      precision_mac = precision_score(dev_labels_pca, predictions, average='macro')
+      recall_mac = recall_score(dev_labels_pca, predictions, average='macro')
+      f1_mac = f1_score(dev_labels_pca, predictions, average='macro')
+      print("\nMacro-average quality numbers for OVR Logistic Regression PCA'd data")
+      print("Precision: {:.4f}, Recall: {:.4f}, F1-measure: {:.4f}".format(precision_mac, recall_mac, f1_mac))
 
-      print("\nClassification Report")
+      print("\nClassification Report for OVR Logistic Regression PCA'd data:")
       print (metrics.classification_report(dev_labels_pca, predictions))
 
       # Try GridSearchCV, Binary Relevance with Logistic Regression
 
+      print("Moving to Grid Search CV for Logistic Regression for PCA'd data")
       parameters = [
         {
               'classifier': [LogisticRegression(multi_class="auto", tol=0.015, solver="liblinear")],
-              'classifier__C': [0.01, 0.1, 1.0,2.0, 10.0, 100],
+              'classifier__C': [1.0e-10, 0.0001, 0.001, 0.01, 0.1, 0.5, 1.0, 2.0, 10.0, 100],
               'classifier__max_iter': [25,50]
          },
 
       ]
 
+      # Grid Search Binary Relevance Logistic Regression
       clf = GridSearchCV(BinaryRelevance(), parameters, cv = 5, scoring='f1_micro')
-      print("Starting Fit")
+      print("Starting Fit of Grid Search for Binary Relevance")
       clf.fit(train_data_pca, train_labels_pca)
 
-      print("Here are the best settings")
+      print("Here are the best settings for Binary Relevance")
       print (clf.best_params_, clf.best_score_)
 
+      # Try GridSearchCV, Label Powerset with Logistic Regression
+
+      clf_labelpower = GridSearchCV(LabelPowerset(), parameters, cv = 5, scoring='f1_micro')
+      print("Starting Fit of Grid Search for Label Powerset")
+      clf_labelpower.fit(train_data_pca, train_labels_pca)
+
+      print("Here are the best settings for Label Power Set")
+      print (clf_labelpower.best_params_, clf_labelpower.best_score_)
+
+      # Try GridSearchCV, Chain Classifier with Logistic Regression
+      clf_chain = GridSearchCV(ClassifierChain(), parameters, cv = 5, scoring='f1_micro')
+      print("Starting Fit of Grid Search For Classifer Chain")
+      clf_chain.fit(train_data_pca, train_labels_pca)
+
+      print("Here are the best settings for Classifier Chains")
+      print (clf_chain.best_params_, clf_chain.best_score_)
+
+
+
 dim_reduct_transform()
+
+# Now that we have found the best parameters, we will do Logistic Regression with Binary BinaryRelevance
+# Label Powerset and Classifer Chain
+def bin_logreg():
+
+    # Create classifer for Binary Relevance Logistic Regression Using PCA'd data
+    logreg_bin = BinaryRelevance(LogisticRegression(C=.0001,solver="liblinear", multi_class="auto", max_iter=25, tol=0.015))
+    print("Created classifier for Binary Relevance Logistic Regression")
+
+    logreg_bin.fit(train_data_pca, train_labels_pca)
+    print("Fit the classifier for Binary Relevance Logistic Regression")
+
+    # get predictions for dev data to be evaluated
+    pred_bin_log = logreg_bin.predict(dev_data_pca)
+    print("Predicted the model for Binary Relevance Logistic Regression")
+
+    # get the accuracy scores
+    score = accuracy_score(dev_labels_pca, pred_bin_log)
+    print("Accuracy with Binary Relevance Logistic Regression and C={c}: {score}".format(c=.0001, score=score))
+
+    # hamming
+    ham = hamming_loss(dev_labels_pca, pred_bin_log)
+    print("Hamming score with Binary Relevance Logistic Regression with C={c}: {ham_score}".format(c=.0001, ham_score=ham))
+
+    precision = precision_score(dev_labels_pca, pred_bin_log, average='micro')
+    recall = recall_score(dev_labels_pca, pred_bin_log, average='micro')
+    f1 = f1_score(dev_labels_pca, pred_bin_log, average='micro')
+    print("\nMicro-average quality numbers for Binary Relevance and Logistice Regression Using PCA'd data:")
+    print("Precision: {:.4f}, Recall: {:.4f}, F1-measure: {:.4f}".format(precision, recall, f1))
+
+bin_logreg()
+
+
+def power_logreg():
+
+    # Create classifer for Label Powerset Logistic Regression Using PCA'd data
+    logreg_power = LabelPowerset(LogisticRegression(C=2.0,solver="liblinear", multi_class="auto", max_iter=25, tol=0.015))
+    print("Created classifier for Label Powerset Logistic Regression")
+
+    logreg_power.fit(train_data_pca, train_labels_pca)
+    print("Fit the classifier for Label Powerset Logistic Regression")
+
+    # get predictions for dev data to be evaluated
+    pred_power = logreg_power.predict(dev_data_pca)
+    print("Predicted the model for Label Powerset Logistic Regression")
+
+    # get the accuracy scores
+    score_power = accuracy_score(dev_labels_pca, pred_power)
+    print("Accuracy with Label Powerset Logistic Regression and C={c}: {score}".format(c=2.0, score=score_power))
+
+    # hamming
+    ham_power = hamming_loss(dev_labels_pca, pred_power)
+    print("Hamming score with Label Powerset Logistic Regression with C={c}: {ham_score}".format(c=2.0, ham_score=ham_power))
+
+    precision_power = precision_score(dev_labels_pca, pred_power, average='micro')
+    recall_power = recall_score(dev_labels_pca, pred_power, average='micro')
+    f1_power = f1_score(dev_labels_pca, pred_power, average='micro')
+    print("\nMicro-average quality numbers for Label Powerset and Logistice Regression Using PCA'd data:")
+    print("Precision: {:.4f}, Recall: {:.4f}, F1-measure: {:.4f}".format(precision_power, recall_power, f1_power))
+
+power_logreg()
+
+
+def class_logreg():
+
+    # Create classifer for Classifier Chain Logistic Regression Using PCA'd data
+    logreg_chain = ClassifierChain(LogisticRegression(C=0.0001,solver="liblinear", multi_class="auto", max_iter=25, tol=0.015))
+    print("Created classifier for Classifer Chain Logistic Regression")
+
+    logreg_chain.fit(train_data_pca, train_labels_pca)
+    print("Fit the classifier for Classifier Chain Logistic Regression")
+
+    # get predictions for dev data to be evaluated
+    pred_chain = logreg_chain.predict(dev_data_pca)
+    print("Predicted the model for Classifier Chain Logistic Regression")
+
+    # get the accuracy scores
+    score_chain = accuracy_score(dev_labels_pca, pred_chain)
+    print("Accuracy with Classifier Chain Logistic Regression and C={c}: {score}".format(c=0.0001, score=score_chain))
+
+    # hamming
+    ham_chain = hamming_loss(dev_labels_pca, pred_chain)
+    print("Hamming score with Classifier Chain Logistic Regression with C={c}: {ham_score}".format(c=0.0001, ham_score=ham_chain))
+
+    precision_chain = precision_score(dev_labels_pca, pred_chain, average='micro')
+    recall_chain = recall_score(dev_labels_pca, pred_chain, average='micro')
+    f1_chain = f1_score(dev_labels_pca, pred_chain, average='micro')
+    print("\nMicro-average quality numbers for Classifier Chain and Logistice Regression Using PCA'd data:")
+    print("Precision: {:.4f}, Recall: {:.4f}, F1-measure: {:.4f}".format(precision_chain, recall_chain, f1_chain))
+
+class_logreg()
